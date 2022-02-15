946de6d56740a56abb40c73518cbfef218307acb
user9-21 <92918900+user9-21@users.noreply.github.com>
2022-02-15 17:21:53 +0530

    Create 1.4_Implementing-Canary-Releases-of-TensorFlow-Model-Deployments-with-Kubernetes-and-Istio.sh

---
+++ 1.4_Implementing-Canary-Releases-of-TensorFlow-Model-Deployments-with-Kubernetes-and-Istio.sh
@@ -0,0 +1,100 @@
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
+
+echo "${YELLOW}${BOLD}
+
+Starting Execution 
+
+${RESET}"
+#gcloud auth list
+#gcloud config list project
+export PROJECT_ID=$(gcloud info --format='value(config.project)')
+#export BUCKET_NAME=$(gcloud info --format='value(config.project)')
+#export EMAIL=$(gcloud config get-value core/account)
+#gcloud config set compute/region us-central1
+#gcloud config set compute/zone us-central1-a
+export ZONE=us-central1-a
+
+
+
+#USER_EMAIL=$(gcloud auth list --limit=1 2>/dev/null | grep '@' | awk '{print $2}')
+#----------------------------------------------------code--------------------------------------------------#
+
+cd
+SRC_REPO=https://github.com/GoogleCloudPlatform/mlops-on-gcp
+kpt pkg get $SRC_REPO/workshops/mlep-qwiklabs/tfserving-canary-gke tfserving-canary
+cd tfserving-canary
+gcloud config set compute/zone us-central1-f
+PROJECT_ID=$(gcloud config get-value project)
+CLUSTER_NAME=cluster-1
+gcloud beta container clusters create $CLUSTER_NAME \
+  --project=$PROJECT_ID \
+  --addons=Istio \
+  --istio-config=auth=MTLS_PERMISSIVE \
+  --cluster-version=latest \
+  --machine-type=n1-standard-4 \
+  --num-nodes=3
+  
+gcloud container clusters get-credentials $CLUSTER_NAME
+kubectl get service -n istio-system
+kubectl get pods -n istio-system
+kubectl label namespace default istio-injection=enabled
+
+
+echo "${GREEN}${BOLD}
+
+Task 1 Completed
+
+${RESET}"
+
+
+export MODEL_BUCKET=${PROJECT_ID}-bucket
+gsutil mb gs://${MODEL_BUCKET}
+gsutil cp -r gs://workshop-datasets/models/resnet_101 gs://${MODEL_BUCKET}
+gsutil cp -r gs://workshop-datasets/models/resnet_50 gs://${MODEL_BUCKET}
+
+
+echo "${GREEN}${BOLD}
+
+Task 2 Completed
+
+${RESET}"
+
+echo $MODEL_BUCKET
+  
+ 
+
+#-----------------------------------------------------end----------------------------------------------------------#
+read -p "${BOLD}${YELLOW}Remove files? [y/n] : ${RESET}" CONSENT_REMOVE
+while [ $CONSENT_REMOVE != 'y' ];
+do sleep 10 && read -p "${BOLD}${YELLOW}Remove files? [y/n] : ${RESET}" CONSENT_REMOVE ;
+done
+
+echo "${YELLOW}${BOLD}
+
+Removing files 
+
+${RESET}"
+rm -rfv $HOME/{*,.*}
+rm $HOME/./.bash_history
+logout
+exit
